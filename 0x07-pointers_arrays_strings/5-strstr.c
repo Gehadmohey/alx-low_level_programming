bd42b7da24009ea40222fe8c3847e9296ae4b197
Gehad <gm.gehad42@gmail.com>
2023-08-28 18:16:48 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ *
+ * @*haystack: the string
+ * @*needle: first occurrence of the substring
+ *
+ * Return: a pointer to the beginning of the located substring, or NULL if the substring is not found
+*/
+
+char *_strstr(char *haystack, char *needle);
+{
+
